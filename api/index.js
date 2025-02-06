3535984f37ec1f68cc9b23e9983ec43b935fe97b
Marko Strba <mark3strba@gmail.com>
2025-02-06 13:19:06 +0100

    feat(auth): add profile endpoint and auto-login after registration
    - Created a /profile endpoint to verify JWT and fetch user info.
    - Added user auto-login on registration by setting user context and making an API request to /profile.
    - Persisted JWT token in cookies for session management across page refreshes.

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ import { config } from "dotenv";
 import User from "./models/User.js";
 import jwt from "jsonwebtoken";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 config();
 connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
@@ -19,14 +20,35 @@ app.use(
 );
 const PORT = 4000;
 
+app.get("/profile", async (req, res) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(400).json({ message: "JWT token is missing" });
+  }
+  jwt.verify(token, jwtSecret, {}, (err, user) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    res.json(user);
+  });
+});
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
     const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).status(201).json({ _id: createdUser._id });
-    });
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({ _id: createdUser._id });
+      }
+    );
   } catch (err) {
     if (err) throw err;
   }
