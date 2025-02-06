2a36b20107ee368cf1c7931c9acd9ca85a6f1ff4
Marko Strba <mark3strba@gmail.com>
2025-02-06 14:59:56 +0100

    feat: add login route and implement bcrypt password hashing with salt

--- api/index.js
+++ api/index.js
@@ -5,9 +5,12 @@ import User from "./models/User.js";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bcrypt from "bcryptjs";
 config();
 connect(process.env.MONGO_URL);
+
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(express.json());
@@ -33,10 +36,34 @@ app.get("/profile", async (req, res) => {
   });
 });
 
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token, { sameSite: "none", secure: true }).json({
+            id: foundUser._id,
+          });
+        }
+      );
+    }
+  }
+});
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const createdUser = await User.create({ username, password });
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      username: username,
+      password: hashedPassword,
+    });
     jwt.sign(
       { userId: createdUser._id, username },
       jwtSecret,
